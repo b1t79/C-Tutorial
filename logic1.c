9b7011813d18d721f304d37e12a901a16a7e5e22
b1t79 <131845430+b1t79@users.noreply.github.com>
2024-09-12 08:55:01 +0530

    commit

--- logic1.c
+++ logic1.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 int main () {
-    printf("%d \n", 3>4 && 5>2)
+    printf("%d \n", 3>4 && 5>2);
+    printf("%d \n", 3>4 || 5>2);
+    printf("%d \n", !( (3>4) && (7<1)));
+    return 0;
 }
